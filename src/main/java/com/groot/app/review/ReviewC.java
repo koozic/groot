42225266fde1bbd09587af3cb69ad0046e919f77
cw <cw>
2026-04-02 16:42:22 +0900

    first commit

---
+++ src/main/java/com/groot/app/review/ReviewC.java
@@ -0,0 +1,17 @@
+package com.groot.app.review;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ReviewC", value = "/review")
+public class ReviewC extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/com/groot/app/user/UserC.java
@@ -0,0 +1,17 @@
+package com.groot.app.user;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserC", value = "/user")
+public class UserC extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    public void destroy() {
+    }
+}
