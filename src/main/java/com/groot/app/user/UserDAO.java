3e48f018c6e81738ee25eb6f7a62d69b0065fef2
cw <cw>
2026-04-09 17:17:49 +0900

    CHAGNED 5:17

--- src/main/java/com/groot/app/user/UserDAO.java
+++ src/main/java/com/groot/app/user/UserDAO.java
@@ -180,15 +180,15 @@ public class UserDAO {
             // =========================
 
             String selectedProfile = request.getParameter("default_profile");   // 라디오 선택값
-            Part profileFile = request.getPart("user_profile");            // 파일 업로드
-
+            String profileImg= (String) request.getAttribute("user_profile");
             String finalProfilePath = null;
 
             // 직접 업로드가 있으면 Cloudinary 우선
-            if (profileFile != null && profileFile.getSize() > 0) {
-
+            if (profileImg != null && !profileImg.trim().isEmpty()) {
+                // finalProfilePath=CloudinaryUtil.uploadFile(profileFile,"user");
+                finalProfilePath = profileImg;
+                // 업로드 없으면 기본 프로필 사용}
             }
-            // 업로드 없으면 기본 프로필 사용
             else if (selectedProfile != null && !selectedProfile.trim().isEmpty()) {
                 finalProfilePath = "user/userImg/" + selectedProfile;
                 System.out.println("기본 프로필 선택: " + finalProfilePath);
